614fe4257c2aa50778c015bc8e61caa2ca05cbe8
mentor-oonuki <mentor.oonuki@gmail.com>
2016-10-17 05:16:09 +0900

    【初期登録】

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRig : MonoBehaviour {
+
+    public Transform Target;
+    public float Low;
+    public float High;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        transform.position = new Vector3(Target.position.x, Clamp(Target.position.y, Low, High), transform.position.z);
+	}
+
+    private float Clamp(float value, float low, float high)
+    {
+        return Mathf.Min(Mathf.Max(value, low), high);
+    }
+}
